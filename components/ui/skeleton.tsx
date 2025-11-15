98fb01854c7cb843c2f634fd682259c3d1c9c3e9
Melanialassia <alassiamelani1@gmail.com>
2025-11-15 05:54:54 +0100

    fix build

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,13 +1,13 @@
-import { cn } from "@/lib/utils"
+import classNames from "classnames"
 
 function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-accent animate-pulse rounded-md", className)}
+      className={classNames("bg-accent animate-pulse rounded-md", className)}
       {...props}
     />
-  )
+  );
 }
 
-export { Skeleton }
+export { Skeleton };
